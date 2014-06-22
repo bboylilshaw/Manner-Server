0cc07254e354fa462b0bc2c6dc92233b88bcd4b7
Jason <bboylilshaw@gmail.com>
2014-06-22 23:43:29 +0800

    fixed some small issues

--- src/main/java/com/hp/manner/service/ItemServiceImpl.java
+++ src/main/java/com/hp/manner/service/ItemServiceImpl.java
@@ -3,6 +3,7 @@ package com.hp.manner.service;
 import com.hp.manner.model.Item;
 import com.hp.manner.model.User;
 import com.hp.manner.repository.ItemRepository;
+import com.hp.manner.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,19 +14,19 @@ import java.util.List;
 public class ItemServiceImpl implements ItemService {
 
     @Autowired
-    private UserServiceImpl userService;
+    private UserRepository userRepository;
     @Autowired
     private ItemRepository itemRepository;
 
     @Override
     public List<Item> listItemsByOwner(String userId) {
-        User user = userService.getUser(new ObjectId(userId));
+        User user = userRepository.findOne(new ObjectId(userId));
         return itemRepository.findByOwner(user);
     }
 
     @Override
     public List<Item> listAllItems() {
-        return (List<Item>) itemRepository.findAll();
+        return itemRepository.findAll();
     }
 
     @Override
