bbacf7571658fbd604052880010122d719dac8c7
Tony Zhou <zong-bo.zhou@hp.com>
2014-12-16 17:21:57 +0800

    tony commit

--- src/main/java/com/hp/manner/controller/rest/ReportCtrl.java
+++ src/main/java/com/hp/manner/controller/rest/ReportCtrl.java
@@ -1,20 +1,21 @@
 package com.hp.manner.controller.rest;
 
-import com.hp.manner.common.ResponseTemplate;
-import com.hp.manner.common.TableName;
-import com.hp.manner.repository.GroupRepository;
-import com.hp.manner.repository.ItemRepository;
-import com.hp.manner.repository.UserRepository;
-import com.hp.manner.service.IReportService;
+import java.sql.Date;
+import java.util.List;
+
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.util.List;
+import com.hp.manner.common.ResponseTemplate;
+import com.hp.manner.common.TableName;
+import com.hp.manner.repository.GroupRepository;
+import com.hp.manner.repository.ItemRepository;
+import com.hp.manner.repository.UserRepository;
+import com.hp.manner.service.IReportService;
 
 @RestController
 @RequestMapping(value="/api",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
@@ -26,14 +27,20 @@ public class ReportCtrl {
 	@Autowired
 	GroupRepository groupRepository;
 	@Autowired
-	Connection conn;
-	@Autowired
 	IReportService reportService;
 
 	@RequestMapping(value="/report")
 	public ResponseTemplate getReport(Long userId,Date date){
-		List<Object> listReport=reportService.retrieve(userId, date);
 		ResponseTemplate response=new ResponseTemplate();
+		if(userId==null){
+			response.setSuccessful(false);
+			response.setMessage("no userId");
+			return response;
+		}
+		if(date==null){
+			date=(Date) DateTime.now().toDate();
+		}
+		List<Object> listReport=reportService.retrieve(userId, date);
 		response.addData(TableName.Report, listReport);
 		return response;
 	}
